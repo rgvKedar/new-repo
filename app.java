439ded82039b344d7689154fa9ae3cd896682a9f
root <root@ip-172-31-67-37.ec2.internal>
2026-03-06 00:07:25 +0000

    data added

---
+++ app.java
@@ -0,0 +1,9 @@
+// The class name must match the filename (e.g., "HelloWorld.java")
+public class HelloWorld {
+    // The main method is the entry point for all Java applications
+    public static void main(String[] args) {
+        // System.out.println() is used to print output
+        System.out.println("Hello, World!");
+    }
+}
+
